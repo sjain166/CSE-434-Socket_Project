980d8e6240af42dfe188de8a6fe7fd7c38e3608d
Kushal Paliwal <kp21official@gmail.com>
2023-02-15 17:32:25 -0700

    Update server.java
    Test code for Server.java

--- server.java
+++ server.java
@@ -1,5 +1,10 @@
+import java.net.*;
+import java.io.*;
 public class server{
-	public static void main(String args[]){
-		System.out.println("Hello Server");
+	public static void main(String args[]) throws IOException{
+            ServerSocket ss = new ServerSocket(4999);
+            Socket s = ss .accept();
+            
+            System.out.println("Client connected");
 	}
 }
