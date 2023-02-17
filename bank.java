29a45ad63bdb63d7b6391922fca216f38dd3c48b
Siddharth Jain <85589093+sjain166@users.noreply.github.com>
2023-02-16 19:06:28 -0700

    two-way-connectivity-done
    -SJ

--- bank.java
+++ bank.java
@@ -1,16 +1,96 @@
+
 import java.net.*;
 import java.io.*;
-public class bank{
-	public static void main(String args[]) throws IOException{
-            ServerSocket ss = new ServerSocket(4999);
-            Socket s = ss .accept();
-            
-            System.out.println("Client connected");
-            
-            InputStreamReader in = new InputStreamReader(s.getInputStream());
-            BufferedReader bf = new BufferedReader(in);
-            
-            String str = bf.readLine();
-            System.out.println(str);
-	}
+
+
+
+public class bank {
+
+    public static void main(String args[]) throws IOException {
+        ServerSocket server = null;
+
+        try {
+
+            // server is listening on port 1234
+            server = new ServerSocket(1234);
+            server.setReuseAddress(true);
+
+            // running infinite loop for getting
+            // client request
+            while (true) {
+
+                // socket object to receive incoming client
+                // requests
+                Socket client = server.accept();
+
+                // Displaying that new client is connected
+                // to server
+                System.out.println("New client connected"+ client.getInetAddress().getHostAddress());
+
+                // create a new thread object
+                ClientHandler clientSock = new ClientHandler(client);
+
+                // This thread will handle the client
+                // separately
+                new Thread(clientSock).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+
+        private final Socket clientSocket;
+
+        // Constructor
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            PrintWriter out = null;
+            BufferedReader in = null;
+            try {
+
+                // get the outputstream of client
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                // get the inputstream of client
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String line;
+                while ((line = in.readLine()) != null) {
+
+                    // writing the received message from
+                    // client
+                    System.out.printf(" Sent from the client: %s\n",line);
+                    out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                    if (in != null) {
+                        in.close();
+                        clientSocket.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
--- customer.java
+++ customer.java
@@ -1,20 +1,42 @@
 
 import java.io.IOException;
-import java.net.Socket;
 import java.io.*;
 import java.util.Scanner;
+import java.net.*;
 
-public class customer{
-    public static void main(String args[]) throws IOException{
-            Socket newSocket = new Socket("localhost" , 4999);
-            
+public class customer {
+
+    public static void main(String args[]) throws IOException {
+        try ( Socket socket = new Socket("localhost", 1234)) {
+
+            // writing to server
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            // reading from server
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            // object of scanner class
             Scanner sc = new Scanner(System.in);
-            System.out.println("Register a New Connection :");
-            String msg = sc.nextLine();
-            
-            PrintWriter pw = new PrintWriter(newSocket.getOutputStream());
-            pw.println(msg);
-            pw.flush();
-            
+            String line = null;
+
+            while (!"exit".equalsIgnoreCase(line)) {
+
+                // reading from user
+                line = sc.nextLine();
+
+                // sending the user input to server
+                out.println(line);
+                out.flush();
+
+                // displaying server reply
+                System.out.println("Server replied "+ in.readLine());
+            }
+
+            // closing the scanner object
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
